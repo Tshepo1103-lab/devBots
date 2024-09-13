ac454956b00c15a7c107891f6eb3fe01181d5bd7
Tshepo-Mahlangu-boxfusion <159532803+Tshepo-Mahlangu-boxfusion@users.noreply.github.com>
2024-09-13 17:46:36 +0200

    chore: added the getAllStats

---
+++ aspnet-core/src/app.Application/Services/Timesheet/Dto/Read/summaryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.Services.Timesheet.Dto.Read
+{
+    public class summaryDto
+    {
+        public int? TotalUserHours { get; set; }
+        public long DaysUserWorked { get; set; }
+    }
+}
